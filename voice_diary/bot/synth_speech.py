8bce2c01431af96bbbfcf064946944ce233d875a
Konstantin Kutsenko <ssidein@gmail.com>
2022-04-12 11:03:41 +0300

    Update synth_speech.py

--- voice_diary/bot/synth_speech.py
+++ voice_diary/bot/synth_speech.py
@@ -16,8 +16,8 @@ def text_to_speech(text):
     data = {
         'text': text,
         'lang': 'ru-RU',
-        'voice': 'filip', #filip, alena, jane
-        #'emotion' : 'good' #neutral
+        'voice': 'filipp', #alena, jane
+        'emotion' : 'good' #neutral
     }
 
     with requests.post(url, headers=header, data=data, stream=True) as resp:
