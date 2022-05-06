8604b531de8ee2dbc441ecd1b28f3d2b41464013
Konstantin Kutsenko <ssidein@gmail.com>
2022-05-06 20:06:12 +0300

    Create upload_test.py

---
+++ voice_diary/bot/test/upload_test.py
@@ -0,0 +1,19 @@
+
+
+import requests
+
+
+local_file_path = 'local_z.ogg'
+
+upload_url = "http://127.0.0.1:8000/"
+
+data = open(local_file_path, 'rb').read()
+headers = {
+    "Content-Type":"application/binary",
+}
+upload = requests.put(upload_url,data=data,headers=headers)
+
+
+print("Upload done: ", upload)
+
+print("DONE!")
