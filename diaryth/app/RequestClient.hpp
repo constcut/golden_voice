196d158fed1ed2f709efd527215398d8dcf6d6cc
Konstantin Kutsenko <ssidein@gmail.com>
2022-05-11 15:08:20 +0300

    Update RequestClient.hpp

--- diaryth/app/RequestClient.hpp
+++ diaryth/app/RequestClient.hpp
@@ -12,13 +12,13 @@ namespace diaryth {
         Q_OBJECT
     public:
 
-        Q_INVOKABLE bool logIn(QString username, QString password);
+        Q_INVOKABLE bool logIn(QString username, QString password) {}
 
-        Q_INVOKABLE void sendAudioFile(QString filename);
-        Q_INVOKABLE void sendPhotoFile(QString filename);
+        Q_INVOKABLE void sendAudioFile(QString filename) {}
+        Q_INVOKABLE void sendPhotoFile(QString filename) {}
 
-        Q_INVOKABLE bool requestCompleteStatus(QString id, QString key);
-        Q_INVOKABLE QString getLastCompleteAnswer();
+        Q_INVOKABLE bool requestCompleteStatus(QString id, QString key) {}
+        Q_INVOKABLE QString getLastCompleteAnswer() {}
 
     private:
 
