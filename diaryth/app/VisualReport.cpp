106018afc5b45107cd13a9e0ad9d69c5ae8e684d
Konstantin Kutsenko <ssidein@gmail.com>
2022-05-13 17:20:58 +0300

    Update VisualReport.cpp

--- diaryth/app/VisualReport.cpp
+++ diaryth/app/VisualReport.cpp
@@ -34,6 +34,9 @@ void VisualReport::paint(QPainter* painter)
 
     qDebug() << "Events total count: " << events.size();
 
+
+    const double zoomCoef = 200.0;
+
     for (const auto& e: events)
     {
         auto eObj = e.toObject();
@@ -52,12 +55,24 @@ void VisualReport::paint(QPainter* painter)
         if (type == "word") {
             y += 50;
             word = eObj["word"].toString();
+
+
+            auto pitch = eObj["stats"].toObject()["praat_pitch"].toObject();
+
+            qDebug() << pitch["mean"].toDouble() << pitch["median"].toDouble()
+                     << pitch["min"].toDouble() << pitch["max"].toDouble()
+                     << pitch["SD"].toDouble()  ;
+
+
+            painter->drawRect(start * zoomCoef + 5, y, (end - start) * zoomCoef, rectWidth);
         }
+        else
+            painter->drawRect(start * zoomCoef + 5, y, (end - start) * zoomCoef, rectWidth);
 
         const double zoomCoef = 200.0;
         const double rectWidth = 20;
 
-        painter->drawRect(start * zoomCoef + 5, y, (end - start) * zoomCoef, rectWidth);
+
 
         if (type == "word")
             painter->drawText(start * zoomCoef + 5, y + rectWidth - 3, word);
