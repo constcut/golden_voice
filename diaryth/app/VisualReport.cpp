acf33dc52635b0b391c7150b08564c37538ff5cd
Konstantin Kutsenko <ssidein@gmail.com>
2022-05-13 14:05:44 +0300

    Update VisualReport.cpp

--- diaryth/app/VisualReport.cpp
+++ diaryth/app/VisualReport.cpp
@@ -3,7 +3,9 @@
 #include <QDebug>
 #include <QJsonArray>
 #include <QJsonDocument>
+#include <QJsonObject>
 #include <QFile>
+
 
 using namespace diaryth;
 
@@ -11,9 +13,30 @@ using namespace diaryth;
 
 void VisualReport::paint(QPainter* painter)
 {
-    QFile f; //full report
-    QJsonDocument doc;
-    QJsonArray events;
+    QFile f = QFile("C:/Users/constcut/Desktop/local/full_report.json"); //full report
+
+    f.open(QIODevice::ReadOnly);
+
+    if (f.isOpen())
+        qDebug() << "JSON File opened";
+
+    QJsonDocument doc = QJsonDocument::fromJson(f.readAll());
+
+    auto root = doc.object();
+    QJsonArray events = root["events"].toArray();
+
+    qDebug() << "Events total count: " << events.size();
+
+    for (const auto& e: events)
+    {
+        auto eObj = e.toObject();
+
+        auto type = eObj["type"].toString();
+        double start = eObj["start"].toDouble();
+        double end = eObj["end"].toDouble();
+
+        qDebug() << "Type " << type << " start " << start << " end " << end;
+    }
 
     //Draw silence as blue, and word as green
     //x is time*coef = 100, for a start
