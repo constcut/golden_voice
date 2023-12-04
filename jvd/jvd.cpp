8e2330072801e8f3a10e06143fcbbc0171e51448
Konstantin Kutsenko <ssidein@gmail.com>
2023-12-05 01:02:16 +0300

    Jvd basis + clang format

--- jvd/jvd.cpp
+++ jvd/jvd.cpp
@@ -1,100 +1,96 @@
 #include <algorithm>
+#include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-#include "json.hpp"
-
-#include "simdjson.h"
 #include "simdjson.cpp"
-
-namespace fs = std::filesystem;
-
+#include "simdjson.h"
 
 // Подробная статистика по каждому слову
+// Отдельный класс
 // # в череде глобальных слов, праат, длительность, диапазоны высоты и грокомости
 
+using namespace simdjson;
 
 class RecordsManager {
  public:
-  RecordsManager() = default;
-  
-  void add_json_record(const fs::path& json_path) {
+  RecordsManager() {
+    paths_.reserve(500);
+    records_.reserve(500);
+  }
+
+  void add_json_record(const std::filesystem::path& json_path) {
+    auto str = padded_string::load(json_path.c_str());
+    ondemand::parser parser;
+    ondemand::document data = parser.iterate(str);
     paths_.push_back(json_path);
-    std::ifstream file(json_path);
-    auto data = nlohmann::json::parse(file);
-    for (const auto& stat: stats_list_) {
-        stats_[stat] += static_cast<double>(data[stat]);
-        const std::string date = data["date"];
-        dates_[date].push_back(&data);
-        //weeks_;
-        //mothes_;
-        //years_;
+
+    for (const auto& stat : stats_list_) {
+      stats_[stat] += data[stat].get_double();
+      const std::string date = std::string(data["date"].get_string().take_value());
+      dates_[date].push_back(&data);
+      // std::time etc // weeks_; // mothes_; // years_;
     }
-    for (const auto& event: data["events"]) {
-      if (event["type"] == "word") {
-        std::string word = event["word"];
+
+    for (auto event : data["events"]) {
+      const std::string type = std::string(event["type"].get_string().take_value());
+      if (type == "word") {
+        std::string word = std::string(event["word"].get_string().take_value());
         std::transform(word.begin(), word.end(), word.begin(), ::tolower);
         words_[word].push_back(&event);
+        // TODO we don't have here exact time of each word!!!
+        // TODO total stats:
+            // letters freq, duration
+            // stats - praat_pitch, intensiti -> word "range"
+            // full praat info -> Jitter, Shimmer
       }
     }
+
     records_.push_back(std::move(data));
-    // by_date_[data["date"]].push_back(&data);
-    // by_month_["october"], by_week_day_["monday"]...
-
-    // cycle - each event: pause or word
-    // part of speech
-    //
-
-    // letters freq, duration
-    // stats - praat_pitch, intensiti -> word "range"
-    // full praat info -> Jitter, Shimmer
-
-    //
-    //
-    //by_word_["word"].push_back(events[17])
-
-    // TODO calculate each word, to make top frequent
   }
 
   void print_stats() {
-    for (const auto& [name, value]: stats_) {
-        std::cout << name << " = " << value << ";" << std::endl;
+    for (const auto& [name, value] : stats_) {
+      std::cout << name << " = " << value << ";" << std::endl;
     }
-    for (const auto& [name, records]: words_) {
+    for (const auto& [name, records] : words_) {
       if (records.size() > 50) {
-        std::cout << name << " got " << records.size() << std::endl;
+        std::cout << records.size() << " times " << name  << std::endl;
       }
     }
   }
 
  private:
-  std::vector<fs::path> paths_;
-  std::vector<nlohmann::json> records_; //  TODO make nice naming dictionay
+  std::vector<std::filesystem::path> paths_;
+  std::vector<ondemand::document> records_;
   std::unordered_map<std::string, double> stats_;
 
-  std::map<std::string, std::vector<const nlohmann::json*>> words_;
-  std::map<std::string, std::vector<const nlohmann::json*>> dates_;
-  std::vector<std::vector<const nlohmann::json*>> weeks_;
+  std::map<std::string, std::vector<const simdjson_result<ondemand::value>*>> words_;
+  std::map<std::string, std::vector<const ondemand::document*>> dates_;
+  std::vector<std::vector<const ondemand::document*>> weeks_;
 
   const std::vector<std::string> stats_list_ = {"duration", "words_count", "words_duration", "pauses_duration"};
 };
 
-
 int main() {
-    RecordsManager man;
-    using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
-    for (const auto& it : recursive_directory_iterator("/home/constcut/life/jvd/files")) { // fs::current_path()
-        const auto report_file = it.path();
-        if (report_file.extension() == ".json") {
-            //std::cout << report_file << std::endl;
-            man.add_json_record(report_file);
-        }
+  RecordsManager man;
+  const auto start{std::chrono::steady_clock::now()};
+  using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
+  for (const auto& it : recursive_directory_iterator("/home/constcut/life/jvd/files")) {  // fs::current_path()
+    const auto report_file = it.path();
+    if (report_file.extension() == ".json") {
+      // std::cout << report_file << std::endl;
+      man.add_json_record(report_file);
     }
-    man.print_stats();
-    return 0;
+  }
+  const auto end{std::chrono::steady_clock::now()};
+  const std::chrono::duration<double> elapsed_seconds{end - start};
+  std::cout << "Time spent: " << elapsed_seconds.count() << std::endl;
+  man.print_stats();
+  return 0;
 }
-
